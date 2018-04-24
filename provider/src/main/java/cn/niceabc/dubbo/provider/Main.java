755c26430401d243570315c65820b96e2cc696db
mashiguang0833@sina.com <mashiguang0833@sina.com>
2018-04-24 13:31:39 +0800

    CommandLineRunner

--- provider/src/main/java/cn/niceabc/dubbo/provider/Main.java
+++ provider/src/main/java/cn/niceabc/dubbo/provider/Main.java
@@ -1,5 +1,8 @@
 package cn.niceabc.dubbo.provider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
@@ -8,11 +11,16 @@ import java.io.IOException;
 
 @SpringBootApplication
 @ImportResource("provider.xml")
-public class Main {
+public class Main implements CommandLineRunner{
+    private static Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(Main.class, args);
+    }
 
-        System.in.read();
+    @Override
+    public void run(String... args) throws Exception {
+        log.debug("running.");
+        Thread.currentThread().join();
     }
 }
