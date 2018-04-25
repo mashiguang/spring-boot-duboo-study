5d9e8cc0b4a3d3594b9dcb778e07a7614fca31a8
mashiguang0833@sina.com <mashiguang0833@sina.com>
2018-04-25 15:30:00 +0800

    dubbo.properties

--- consumer/src/main/java/cn/niceabc/dubbo/consumer/Main.java
+++ consumer/src/main/java/cn/niceabc/dubbo/consumer/Main.java
@@ -9,9 +9,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ImportResource("consumer.xml")
+@PropertySource("dubbo.properties")
 public class Main implements CommandLineRunner {
     private static Logger log = LoggerFactory.getLogger(Main.class);
 
--- provider/src/main/java/cn/niceabc/dubbo/provider/Main.java
+++ provider/src/main/java/cn/niceabc/dubbo/provider/Main.java
@@ -6,11 +6,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 import java.io.IOException;
 
 @SpringBootApplication
 @ImportResource("provider.xml")
+@PropertySource("dubbo.properties")
 public class Main implements CommandLineRunner{
     private static Logger log = LoggerFactory.getLogger(Main.class);
 
